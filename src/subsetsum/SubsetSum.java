1df7c206480ad0a2da70a6108fad1a001d088cf9
Joel Reyes <its.j.reyes@gmail.com>
2020-04-23 11:47:35 -0700

    Created method for findSubset

--- src/subsetsum/SubsetSum.java
+++ src/subsetsum/SubsetSum.java
@@ -3,20 +3,43 @@ package subsetsum;
 import java.util.ArrayList;
 
 public class SubsetSum {
-    public ArrayList<Double> findSubset(ArrayList<Double> set, double limit){
+    public ArrayList<Double> findSubset(ArrayList<Double> set, double limit)
+    {
         ArrayList<ArrayList<Double>> col = new ArrayList<ArrayList<Double>>();
         ArrayList<Double> empty = new ArrayList<Double>();
+        ArrayList<Double> lAdd = new ArrayList<Double>();
         ArrayList<Double> lPrime = new ArrayList<Double>();
+        double largest = 0 ;
+        int largestIndex = 0;
         col.add(empty);
         for (int i = 0; i < set.size(); i++)
         {
          for (int j = 0; j < col.size(); j++)
          {
+             if(sumArrayList(col.get(j)) +set.get(i) < limit)
+             {
+                 lAdd = col.get(j);
+                 lAdd.add(set.get(i));
+                 col.add(lAdd);
+             }
+             if(sumArrayList(col.get(j)) +set.get(i) == limit)
+             {
+                 lAdd = col.get(j);
+                 lAdd.add(set.get(i));
+                 lPrime =lAdd;
+                 break;
+             }
          }
         }
-
-        return lPrime;
-
+        for (int k = 0; k<col.size(); k++)
+        {
+            if (sumArrayList(col.get(k)) > largest)
+            {
+                largest = sumArrayList(col.get(k));
+                largestIndex = k;
+            }
+        }
+        return col.get(largestIndex);
     }
     public double sumArrayList(ArrayList<Double> set)
     {
