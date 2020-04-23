1d58929461495caf79c8bf3fa0c8db5c8571f53d
Joel Reyes <its.j.reyes@gmail.com>
2020-04-23 08:51:38 -0700

    Created helper method to calculate sum of ArrayList

--- src/subsetsum/SubsetSum.java
+++ src/subsetsum/SubsetSum.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 
 public class SubsetSum {
     public ArrayList<Double> findSubset(ArrayList<Double> set, double limit){
+        ArrayList<ArrayList<Double>> col = new ArrayList<ArrayList<Double>>();
+        ArrayList<Double> empty = new ArrayList<Double>();
+        ArrayList<Double> lPrime = new ArrayList<Double>();
+        col.add(empty);
+        for (int i = 0; i < set.size(); i++)
+        {
+         for (int j = 0; j < col.size(); j++)
+         {
+         }
+        }
 
+        return lPrime;
+
+    }
+    public double sumArrayList(ArrayList<Double> set)
+    {
+        double sum = 0;
+        for (int i = 0; i > set.size(); i++)
+            sum =+set.get(i);
+        return sum;
     }
 }
